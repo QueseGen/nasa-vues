dc20a2b6114947b699c665e519f0bbcfcb9db940
Jacquese <jacquesewilson@gmail.com>
2022-03-21 11:43:12 -0400

    Testing

---
+++ views/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+ <h1>Testing</h1>
+    <div v-for="post in posts" v-bind:key="post.title">
+
+      <h2>{{ post.title}}</h2> <p>{{ post.date }}</p>
+      <p style="box-sizing: content-box; text-indent: 40px;">{{ post.explanation }}</p> <br>
+      <img v-bind:src=post.hdurl>
+      <p>Reference:<br> {{post.copyright}} <br> {{post.url}} <br> </p>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  el: '#app',
+  data() {
+    return {
+      posts: [],
+      errors:[]
+    };
+  },
+  methods: {
+    async getData() {
+      try {
+        const response = await axios.get(
+          "https://api.nasa.gov/planetary/apod?api_key=ZtGGTCYJAN4niycyVPz6Dn3IyBuVwbNId4G6dpWJ"
+        );
+        // JSON responses are automatically parsed.
+        this.posts.push(response.data);
+      } catch (error) {
+        this.errors.push(error);
+      }
+      console.log(this.posts[0].title);
+    },
+  },
+  created() {
+    this.getData();
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
