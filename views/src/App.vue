f2340e969e2c9f50f8b8d49a44e3dcb6e0bf1fc3
Jacquese <jacquesewilson@gmail.com>
2022-03-26 20:13:51 -0400

    Adjusting styles for external

--- views/src/App.vue
+++ views/src/App.vue
@@ -1,4 +1,4 @@
-<template>
+<template class="app">
   <img alt="Vue logo" src="./assets/logo.png">
   <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
   <h1>Testing</h1>
@@ -45,14 +45,4 @@ export default {
 };
 </script>
 
-<style>
 
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
