3b0b326570babac481a4359759b7f53d3cc30003
QueseGen <jacquesewilson@gmail.com>
2022-04-06 21:05:05 -0400

    testing environment

--- views/src/components/Testing.vue
+++ views/src/components/Testing.vue
@@ -28,7 +28,7 @@ export default {
     async getData() {
       try {
         const response = await axios.get(
-          process.env.VUE_APP_NASA_STORY
+          process.env.NASA_STORY
         );
         // JSON responses are automatically parsed.
         this.posts.push(response.data);
